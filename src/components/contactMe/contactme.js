fa9aa9f1d1e282d67d0d0dba69797af42bc01be7
vinoth sharma <sharmapsgtech@gmail.com>
2020-12-15 20:40:03 +0530

    user details - done

--- src/components/contactMe/contactme.js
+++ src/components/contactMe/contactme.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./contactme.scss";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from '@material-ui/icons/GitHub';
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 export const ContactMe = () => {
   return (
     <div>
-      <p>Connect with me</p>
+      {/* <p>Reach Me</p> */}
       <div className="contact-me">
-        <InstagramIcon />
-        <LinkedInIcon />
-        <GitHubIcon />
+        <a href="https://www.instagram.com/na_tha_vvs/" target="_blank" rel="noreferrer">
+          <InstagramIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/vinoth-sharma" target="_blank" rel="noreferrer">
+          <LinkedInIcon />
+        </a>
+        <a href="https://github.com/vinoth-sharma" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
       </div>
     </div>
   );
--- src/containers/left-section/leftSection.js
+++ src/containers/left-section/leftSection.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { Avator } from '../../components/avatar/avatar';
-import { ContactMe } from '../../components/contactMe/contactme';
-import "./leftSection.scss"
+import React from "react";
+import { Avator } from "../../components/avatar/avatar";
+import { ContactMe } from "../../components/contactMe/contactme";
+import "./leftSection.scss";
 
 function LeftSection() {
   return (
-      <header className="app-header">
-          <Avator />
-          <h2>Vinoth Sharma</h2>
-          <p>Software Developer</p>
-          <ContactMe />
-      </header>
+    <header className="app-header">
+      <Avator />
+      <div className="details">
+        <h2>Vinoth Sharma</h2>
+        <p>Software Developer</p>
+        <ContactMe />
+      </div>
+    </header>
   );
 }
-
-
-
 
 export default LeftSection;
